9154bf652defb77da7546af76454dcb4fc25e495
Alexander Kenrick Duanto <alexanderkenrick3@gmail.com>
2024-12-22 21:45:03 +0700

    Setup template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,24 +3,21 @@
     Home
 @endsection
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Home /</span> Dashboard</h4>
+    <div class="card">
+        <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    {{ __('You are logged in!') }}
+        <div class="card-body">
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
                 </div>
-            </div>
+            @endif
+            {{ __('You are logged in!') }}
         </div>
     </div>
-</div>
+
+
 <script src="{{asset('assets/vendor/js/bootstrap.js')}}"></script>
 @endsection
 
